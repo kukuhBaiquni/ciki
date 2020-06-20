756220ad96e99700540de1c33874440cbc4c63f2
unknown <kukuh.baiquni@gmail.com>
2020-06-21 04:14:01 +0700

    add font asset

--- App.js
+++ App.js
@@ -7,22 +7,29 @@
 * @flow strict-local
 */
 
-import 'react-native-gesture-handler';
-import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import React from "react";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { font } from "./src/common/font";
 
 const App = () => {
-  return (
-    <>
-      <NavigationContainer>
-        <View>
-          <StatusBar backgroundColor="black" barStyle="light-content" />
-          <Text>APP ENTRY POINT</Text>
-        </View>
-      </NavigationContainer>
-    </>
-  );
+    return (
+        <>
+            <NavigationContainer>
+                <View>
+                    <StatusBar backgroundColor="black" barStyle="light-content" />
+                    <Text style={style.test}>tester</Text>
+                </View>
+            </NavigationContainer>
+        </>
+    );
 };
+
+const style = StyleSheet.create({
+    test: {
+        ...font.regular
+    }
+})
 
 export default App;
---
+++ src/common/font.js
@@ -0,0 +1,14 @@
+export const font = {
+    regular: {
+        fontFamily: "ff_rg",
+        fontWeight: "normal"
+    },
+    italic: {
+        fontFamily: "ff_rg_it",
+        fontWeight: "normal",
+    },
+    bold: {
+        fontFamily: "ff_bd",
+        fontWeight: "normal",
+    }
+};
